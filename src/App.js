f7bebdf0e79ac41e656fc260922e8ffc03b240fa
CLackrone <charisaanne@gmail.com>
2018-07-30 11:52:04 -0400

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from './components/NavBar';
+import Home from './components//Home';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
         <div>
           <NavBar />
+          <Route exact path='/' component={Home} />
         </div>
       </Router>
     );
@@ -16,10 +18,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-<Router>
-  <div>
-    <NavBar />
-  </div>
-</Router>
