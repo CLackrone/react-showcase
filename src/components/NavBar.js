d699635ae736c482ab12c5982b521e8e776a6d67
CLackrone <charisaanne@gmail.com>
2018-07-30 11:47:17 -0400

    adding navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <p>This is a placeholder for the NavBar</p>
+    )
+}
+
+export default NavBar
