522d24b4aa998b1220b9f0bbebb631ee8b2f7e70
CLackrone <charisaanne@gmail.com>
2018-07-30 12:09:27 -0400

    adding links to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <p>This is a placeholder for the NavBar</p>
+    <div>
+      <p>This is a placeholder for the NavBar</p>
+      <Link to='/form'>Form</Link> <p> | </p>
+      <Link to='/login'>Login</Link><p> | </p>
+      <Link to='/index'>Index</Link>
+    </div>
     )
 }
 
 export default NavBar
+
+
+//<Link className="navbar-brand" to="/">Toy Library</Link>
