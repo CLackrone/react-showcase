e07cc5c440ac21f03d8380495fa13ede40bf4aa7
CLackrone <charisaanne@gmail.com>
2018-08-06 12:32:02 -0400

    added form fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      name: '',
+      age: '',
+      location: ''
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return(
-      <p>This is the form page</p>
-      )
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          
+          <div>
+            <label>Name: </label>
+            <input 
+              type='text'
+              onChange={e => this.handleChange(e)}
+              name='name'
+              value={this.state.name}
+            />
+          </div>
+
+          <div>
+            <label>Age: </label>
+            <input 
+              type='text'
+              name='age'
+            />
+          </div>
+
+          <div>
+            <label>Location: </label>
+            <input 
+              type='text'
+              name='location'
+            />
+          </div>
+
+        </div>
+      </form>
+    )
   }
 }
 
