b249ebad63730865fe7d56a7626dddb1057b0d10
CLackrone <charisaanne@gmail.com>
2018-08-06 14:12:59 -0400

    mocked out login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('i was clicked')
+    console.log(this.state)
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return(
-      <div>
-        <p>This is the login page</p>
-      </div>
+      <form>
+        <div>
+
+          <div>
+            <label>Name: </label>
+            <input 
+              type='text'
+              onChange={e => this.handleChange(e)}
+              name='name'
+              value={this.state.name}
+            />
+          </div>
+
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+
+        </div>
+      </form>
       )
   }
 }
